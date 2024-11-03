a787f6cab45e3bc77b7bb36c4b7524276130892c
greeeener <winflok943@gmail.com>
2024-11-03 18:31:51 +0900

    start project

---
+++ mocha/src/main/java/com/project/mocha/MochaApplication.java
@@ -0,0 +1,13 @@
+package com.project.mocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MochaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MochaApplication.class, args);
+	}
+
+}
