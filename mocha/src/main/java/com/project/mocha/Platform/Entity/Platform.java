978a8fab3f41bd25e596f035694208d1341504b2
gs25q <rs13q5@gmail.com>
2024-11-17 14:32:53 +0900

    [create] Platform, User, Storage - Entity 작성 #5

--- mocha/src/main/java/com/project/mocha/Platform/Entity/Platform.java
+++ mocha/src/main/java/com/project/mocha/Platform/Entity/Platform.java
@@ -1,4 +1,25 @@
 package com.project.mocha.Platform.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data //getter,setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access=AccessLevel.PROTECTED)
 public class Platform {
+    @Id
+    @Column(name="platform_id", nullable=false)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="PLATFORM_SEQ")
+    @SequenceGenerator(name="PLATFORM_SEQ", allocationSize=1)
+    private int platformId;
+
+    @Column(name="platform_name", nullable=false, length=20)
+    private String platformName;
+
+    @Column(name="coin_cost", nullable=false)
+    private int coinCost;
+
+    // Platform이 가지고 있는 작품들 리스트를 보관할 필요가 있을까?
 }
--- mocha/src/main/java/com/project/mocha/User/Entity/User.java
+++ mocha/src/main/java/com/project/mocha/User/Entity/User.java
@@ -1,4 +1,32 @@
 package com.project.mocha.User.Entity;
 
+import com.project.mocha.Storage.Entity.Storage;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data //getter,setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access=AccessLevel.PROTECTED)
 public class User {
+    @Id
+    @Column(name="user_id", nullable=false)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="USER_SEQ")
+    @SequenceGenerator(name="USER_SEQ", allocationSize=1)
+    private int userId;
+
+    @Column(name="uid", nullable=false, length=20)
+    private String uid;     // user가 직접 지은 ID
+
+    @Column(name="pwd", nullable=false, length=20)
+    private String pwd;
+
+    @Column(name="is_adult", nullable=false)
+    private boolean isAdult;       // default를 false로?
+
+    @OneToMany(mappedBy="Storage")
+    private List<Storage> StorageList;
 }
