e0896ca90626de34b136f6bc272e0e333133a20e
soobnini <soobiny4339@gmail.com>
2024-12-01 16:02:11 +0900

    creationController 작성

--- mocha/src/main/java/com/project/mocha/Creation/Controller/CreationController.java
+++ mocha/src/main/java/com/project/mocha/Creation/Controller/CreationController.java
@@ -1,5 +1,7 @@
 package com.project.mocha.Creation.Controller;
 
+
+import com.project.mocha.Creation.DTO.*;
 import com.project.mocha.Creation.Entity.Creation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -9,28 +11,43 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/mc/main")
+@RequestMapping("/mc/creation")
 @RequiredArgsConstructor
 @Tag(name="main")
 public class CreationController {
     @Operation(summary = "작품 조회 by CreationId", description = "특정 작품을 조회합니다.")
-    @ApiResponses({
+    /*@ApiResponses({
             @ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Creation.class))),
             @ApiResponse(responseCode = "404", description = "찾을 수 없음")
-    })
-    @GetMapping("/{crationId}")
-    public ResponseEntity<Creation> getCreationByCreationId(
-            @Parameter(description = "작품 ID", required = true) @PathVariable Long crationId) {
-        Optional<Creation> creation = CreationService.getCreation(crationId);
-        return creation.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    })*/
+    @GetMapping("/getCreation/{creationId}")
+    public ResponseEntity<CreationResponse> getCreation(
+            @Parameter(description = "작품 ID", required = true)int creationId) {
+
+        return null;
+    }
+    @Operation(summary = "작품 리스트 검색", description = "작품 리스트를 조회합니다.")
+    @PostMapping("/getCreationList")
+    public ResponseEntity<CreationListResponse> getCreationList(CreationListRequest request, Pageable pageable) {
+        return null;
+    }
+
+    @Operation(summary = "작품 생성", description = "작품을 생성합니다.")
+    @PostMapping("/createCreation")
+    public ResponseEntity<CreationResponse> createCreation(CreateCreationRequest request) {
+        return null;
+    }
+
+    @Operation(summary = "작품 생성", description = "작품을 생성합니다.")
+    @PutMapping("/updateCreation/{creationId}")
+    public ResponseEntity<Void> updateCreation(UpdateCreationRequest request) {
+        return null;
     }
 }
