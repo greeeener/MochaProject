6c1b5f327e424c8436f4fa24aa9062191c607d3a
greeeener <winflok943@gmail.com>
2024-12-01 15:31:41 +0900

    [create] Creator, Platform, Keyword - Controller, Service, Entity 수정 및 Request/Response DTO 추가 #10

---
+++ mocha/src/main/java/com/project/mocha/Creator/DTO/UpdateCreatorRequest.java
@@ -0,0 +1,4 @@
+package com.project.mocha.Creator.DTO;
+
+public class UpdateCreatorRequest {
+}
--- mocha/src/main/java/com/project/mocha/Platform/Service/PlatformService.java
+++ mocha/src/main/java/com/project/mocha/Platform/Service/PlatformService.java
@@ -1,4 +1,20 @@
 package com.project.mocha.Platform.Service;
 
+import com.project.mocha.Platform.DTO.CreatePlatformRequest;
+import com.project.mocha.Platform.DTO.CreatePlatformResponse;
+import com.project.mocha.Platform.DTO.UpdatePlatformRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class PlatformService {
+
+    public CreatePlatformResponse createPlatform(CreatePlatformRequest request) {
+        return null;
+    }
+
+    public CreatePlatformResponse updatePlatform(int id, UpdatePlatformRequest request){
+        return null;
+    }
 }
