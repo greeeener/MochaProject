283f3d90c56e3b1a71b811e5b3d7a3157b35ff7b
greeeener <winflok943@gmail.com>
2025-07-06 15:20:39 +0900

    [edit] Edit Structure #21

---
+++ mocha/fe/src/pages/Test/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h1>테스트 페이지</h1>
+            <p>자 테스트를 시작해볼까</p>
+        </div>
+    );
+};
+
+export default Test;
